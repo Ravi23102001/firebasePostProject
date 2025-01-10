f6bd0443214ddef1b693379b37a83be22d4648c7
Ravi <ravindrapandian2001@gmail.com>
2025-01-10 07:56:50 +0530

    'deploycheck'

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import { getFirestore } from "@firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth";
 
 
 const firebaseConfig = {
@@ -17,6 +17,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth=getAuth(app)
+
+setPersistence(auth, browserLocalPersistence);
 
 
 // setPersistence(auth, browserLocalPersistence)
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeContextProvider } from './context/ThemeContext';
-import { ToastContainer, toast } from 'react-toastify';
-import './index.css';
+import { ToastContainer } from 'react-toastify';
 import { AuthContexProvider } from './context/AuthContext';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
